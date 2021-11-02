cc7f636a38447d52fb95e5f1aa8aed9a3b3f017e
freesudani <mohamed.dahiya@hotmail.com>
2021-11-02 11:46:03 +0700

    Commerce Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,18 +3,7 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running Shoes",
-    price: "$5",
-    image: "",
-  },
-  { id: 2, name: "MacBook", description: "Apple", price: "$10", image: "" },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
