a74ed75dbb45fc44716c49b5816c40a6131c9160
freesudani <mohamed.dahiya@hotmail.com>
2021-11-05 11:41:02 +0700

    Cart Buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,24 +4,33 @@ import useStyles from "./styles";
 import { CartItem } from "./CartItem/CartItem";
 import { Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles();
 
-  const EmptyCart = () => {
+  const EmptyCart = () => (
     <typography variant="subtitle1">
       You have no items inside your shopping cart.
       <Link to="/" className={classes.link}>
         please start adding some items
       </Link>
-    </typography>;
-  };
+    </typography>
+  );
 
   const FilledCart = () => (
     <>
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -36,6 +45,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
