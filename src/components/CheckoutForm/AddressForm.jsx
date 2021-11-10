3258c50ad3e110ca18a98fe061ee3cf4ad3ccca9
freesudani <mohamed.dahiya@hotmail.com>
2021-11-10 09:27:08 +0700

    Fetch Countries

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React , {useState}from "react";
+import React , {useState , useEffect} from "react";
 import {
   InputLabel,
   Select,
@@ -11,7 +11,7 @@ import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "./CustomerTextField";
 import {commerce} from '../../lib/commerce'
 
-const AddressForm = () => {
+const AddressForm = ({checkoutToken}) => {
   const [shippingCountries , setShippingCountries] = useState([]);
   const [shippingCountry , setShippingCountry] = useState('');
   const [shippingSubdivisions , setShippingSubdivisions] = useState([]);
@@ -20,6 +20,21 @@ const AddressForm = () => {
   const [shippingOption , setShippingOption] = useState('');
 
   const methods = useForm();
+
+  const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name}))
+
+  const fetchingShippingCountries = async (checkoutTokenId) => {
+    const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId);
+    setShippingCountries(countries)
+    setShippingCountry(Object.keys(countries)[0])
+    
+  }
+
+  useEffect(() => {
+    fetchingShippingCountries(checkoutToken.id)
+  }, [])
+
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -36,8 +51,9 @@ const AddressForm = () => {
             <FormInput required name="zip" label="ZIP / Postal Code" />
             <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Country</InputLabel>
-              <Select value={} fullWidth onChange={}>
-                <MenuItem key={} value={}>Select Me</MenuItem>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                {countries.map((country) => (<MenuItem key={country.id} value={country.id}>{country.label}</MenuItem>))}
+                
               </Select>
             </Grid>
             <Grid item xs={12} sm={6}>
